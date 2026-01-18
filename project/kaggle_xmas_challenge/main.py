8bdb6b28151e4fc95ca74ce8f87621fd9710efb9
jakubiwaszkiewicz <kkuba.iwaszkiewicz@gmail.com>
2026-01-18 16:00:07 +0100

    it works better for debuging a lot

--- project/kaggle_xmas_challenge/main.py
+++ project/kaggle_xmas_challenge/main.py
@@ -1,5 +1,5 @@
 import math
-import time
+from pathlib import Path
 import yaml
 import numpy as np
 import argparse
@@ -14,9 +14,27 @@ from shapely.ops import unary_union
 from decimal import Decimal, getcontext
 from utils.data_repr import ChristmasTree
 from utils.constants import scale_factor, decimal_precision
+import time
+from collections import defaultdict
+import math
+
+FUNC_STATS = defaultdict(lambda: {"time": 0.0, "calls": 0})
+
+def profile(func):
+    def wrapper(*args, **kwargs):
+        t0 = time.perf_counter()
+        result = func(*args, **kwargs)
+        dt = time.perf_counter() - t0
+        name = func.__qualname__
+        FUNC_STATS[name]["time"] += dt
+        FUNC_STATS[name]["calls"] += 1
+        return result
+    return wrapper
+
 
 getcontext().prec = decimal_precision
 
+@profile
 def format_time(elapsed):
     """Take a time in seconds and return a string hh:mm:ss."""
     elapsed_rounded = int(round(elapsed))
@@ -27,6 +45,7 @@ def format_time(elapsed):
 
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
+@profile
 def load_configuration_from_df(n, existing_df):
     """
     Load existing configuration from submission CSV.
@@ -45,6 +64,7 @@ def load_configuration_from_df(n, existing_df):
 def to_str(x: Decimal):
     return f"s{float(x)}"
 
+@profile
 def clone_trees(trees: list[ChristmasTree]):
     cloned_trees = [tree.clone() for tree in trees]
     return cloned_trees
@@ -102,9 +122,52 @@ class SimulatedAnnealing:
         self.F_rot = Decimal(str(F_rot))
         self.temp_exponent = temp_exponent
 
-    def _temperature_scale(self, Temperature):
-        return Decimal(str((Temperature / self.Tmax) ** self.temp_exponent))
+    @profile
+    def _temperature_scale(self, temperature):
+        return Decimal(str((temperature / self.Tmax) ** self.temp_exponent))
 
+    @profile
+    def get_lengths(self, current_trees):
+        xys = np.concatenate([np.asarray(t.polygon.exterior.xy).T / 1e15 for t in current_trees])
+        min_x, min_y = xys.min(axis=0)
+        max_x, max_y = xys.max(axis=0)
+        return max_x - min_x, max_y - min_y
+
+    @profile
+    def calculate_score(self, trees):
+        a, b = self.get_lengths(trees)
+        longer_side = max(a,b)
+        score = longer_side ** 2 / len(trees)
+        return score
+
+    @profile
+    def has_overlap(self, trees, n=None):
+        """Check for overlap between trees."""
+        if len(trees) <= 1:
+            return False
+        if n is None:
+            # check all trees
+            for i, tree1 in enumerate(trees):
+                for j, tree2 in enumerate(trees):
+                    if i < j:
+                        if tree1.polygon.intersects(tree2.polygon) and not tree1.polygon.touches(tree2.polygon):
+                            return True
+        else:
+            # check overlap of specific tree
+            for i, tree1 in enumerate(trees):
+                if i != n:
+                    if tree1.polygon.intersects(trees[n].polygon) and not tree1.polygon.touches(trees[n].polygon):
+                        return True
+        return False
+
+    @profile
+    def _acceptance_probability(self, current_energy, new_energy, temperature):
+        """Calculate the probability of accepting a new solution."""
+        if new_energy < current_energy:
+            return 1.0
+        return math.exp((current_energy - new_energy) / temperature)
+
+    @profile
     def perturb_tree(self, tree, Temperature):
         """Perturb tree position and angle"""
         old_x, old_y, old_angle = tree.get_params()
@@ -126,44 +189,8 @@ class SimulatedAnnealing:
         tree.set_params(new_x, new_y, new_angle)
         return old_x, old_y, old_angle
 
-    def get_lengths(self, current_trees):
-        xys = np.concatenate([np.asarray(t.polygon.exterior.xy).T / 1e15 for t in current_trees])
-        min_x, min_y = xys.min(axis=0)
-        max_x, max_y = xys.max(axis=0)
-        return max_x - min_x, max_y - min_y
-
-    def calculate_score(self, trees):
-        a, b = self.get_lengths(trees)
-        longer_side = max(a,b)
-        score = longer_side ** 2 / len(trees)
-        return score
-
-    def has_overlap(self, trees, n=None):
-        """Check for overlap between trees."""
-        if len(trees) <= 1:
-            return False
-        if n is None:
-            # check all trees
-            for i, tree1 in enumerate(trees):
-                for j, tree2 in enumerate(trees):
-                    if i < j:
-                        if tree1.polygon.intersects(tree2.polygon) and not tree1.polygon.touches(tree2.polygon):
-                            return True
-        else:
-            # check overlap of specific tree
-            for i, tree1 in enumerate(trees):
-                if i != n:
-                    if tree1.polygon.intersects(trees[n].polygon) and not tree1.polygon.touches(trees[n].polygon):
-                        return True
-        return False
-
-    def _acceptance_probability(self, current_energy, new_energy, temperature):
-        """Calculate the probability of accepting a new solution."""
-        if new_energy < current_energy:
-            return 1.0
-        return math.exp((current_energy - new_energy) / temperature)
-
-    def perturb_translations(self, a, b, Temperature):
+    @profile
+    def perturb_translations(self, a, b, temperature):
         """Perturb tree position and angle"""
         old_a = Decimal(str(copy.copy(a)))
         old_b = Decimal(str(copy.copy(b)))
@@ -171,7 +198,7 @@ class SimulatedAnnealing:
         da = Decimal(str(random.uniform(-self.delta_t, self.delta_t)))
         db = Decimal(str(random.uniform(-self.delta_t, self.delta_t)))
 
-        scale = Decimal(str(Temperature / self.Tmax))
+        scale = Decimal(str(temperature / self.Tmax))
 
         da *= scale
         db *= scale
@@ -180,6 +207,7 @@ class SimulatedAnnealing:
         new_b = old_b + old_b * db
         return new_a, new_b, old_a, old_b
 
+    @profile
     def rotate_all(self, trees):
         """Perturb trees angle"""
         old_angles = []
@@ -191,6 +219,7 @@ class SimulatedAnnealing:
             tree.set_params(x, y, new_angle)
         return trees, old_angles
 
+    @profile
     def translate(self, primitive_trees, a, b, num_cell):
         lattice_trees = []
         cells_width = num_cell[0]
@@ -207,19 +236,20 @@ class SimulatedAnnealing:
                     )
         return lattice_trees
 
-    def custom_log(self, temperature, iteration_at_temperature, current_score, best_score, t0):
-        if iteration_at_temperature == (self.iterations_per_temperature - 1):
-            t1 = format_time(time.time() - t0)
-            print(
-                f"Temperature: {temperature}, Current score: {current_score}, "
-                f"Best score: {best_score:8.5f}, Elapsed Time: {t1}, "
-                f"Num cells: {self.num_cell}",
-                flush=True,
-            )
+    @profile
+    def custom_log(self, temperature, current_score, best_score, no_of_rejections):
+        rejection_pct = 100 * no_of_rejections / self.iterations_per_temperature
+        print(
+            f"Temperature: {temperature}, "
+            f"Current score: {current_score}, "
+            f"Best score: {best_score:8.5f}, "
+            f"Num cells: {self.num_cell}, "
+            f"Rejections: {rejection_pct:.1f}%",
+            flush=True,
+        )
 
+    @profile
     def solve(self):
-        all_scores = []
-        t0 = time.time()
         temperature = self.Tmax
         primitive_trees = clone_trees(self.initial_trees)
 
@@ -234,75 +264,81 @@ class SimulatedAnnealing:
             raise Exception("Initial tree configuration has overlap, implementation Error...")
 
         current_score = self.calculate_score(lattice_trees)
-        best_trees = clone_trees(primitive_trees)
+        best_trees = clone_trees(lattice_trees)
         best_score = current_score
 
         for cooling_step in range(self.nsteps):
+            all_scores = []
+            no_of_rejections = 0
             for iteration_at_temperature in range(self.iterations_per_temperature):
-                move_type = random.randint(0, len(primitive_trees) + 1)
-                if move_type < len(primitive_trees):
-                    old_params = self.perturb_tree(primitive_trees[move_type], temperature)
-                elif move_type == len(primitive_trees):
+
+                move_probability = random.random()
+                perturb_tree_probability = 0.8
+                perturb_translations_probability = 0.8
+                has_overlap_in_iter = False
+
+                if perturb_tree_probability < move_probability:
+                    tree_probability = random.randint(0,1)
+                    old_params = self.perturb_tree(primitive_trees[tree_probability], temperature)
+                    if self.has_overlap(self.translate(primitive_trees, a, b, self.num_cell)):
+                        has_overlap_in_iter = True
+                        no_of_rejections += 1
+                        primitive_trees[tree_probability].set_params(*old_params)
+                elif perturb_translations_probability < move_probability:
                     a, b, old_a, old_b = self.perturb_translations(a, b, temperature)
+                    if self.has_overlap(self.translate(primitive_trees, a, b, self.num_cell)):
+                        has_overlap_in_iter = True
+                        no_of_rejections += 1
+                        a = old_a
+                        b = old_b
                 else:
                     primitive_trees, old_angles = self.rotate_all(primitive_trees)
-
-                if self.has_overlap(self.translate(primitive_trees, a, b, self.num_cell)):
-                    # repair if overlap
-                    if move_type < len(primitive_trees):
-                        primitive_trees[move_type].set_params(*old_params)
-                    elif move_type == len(primitive_trees):
-                        a = old_a
-                        b = old_b
-                    else:
+                    if self.has_overlap(self.translate(primitive_trees, a, b, self.num_cell)):
+                        has_overlap_in_iter = True
+                        no_of_rejections += 1
                         for i, tree in enumerate(primitive_trees):
                             x, y, _ = tree.get_params()
                             tree.set_params(x, y, old_angles[i])
-                    self.custom_log(temperature, iteration_at_temperature, current_score, best_score, t0)
-                    continue
-                else:
+
+                if not has_overlap_in_iter:
                     lattice_trees = self.translate(primitive_trees, a, b, self.num_cell)
+                    new_score = self.calculate_score(lattice_trees)
 
-                new_score = self.calculate_score(lattice_trees)
+                    acceptance_probability = self._acceptance_probability(current_score, new_score, temperature)
+                    acceptance = acceptance_probability > random.random()
 
-                acceptance_probability = self._acceptance_probability(current_score, new_score, temperature)
+                    if acceptance:
+                        current_score = new_score
+                        if new_score < best_score:
+                            best_score = new_score
+                            best_trees = clone_trees(lattice_trees)
 
-                if acceptance_probability > random.random():
-                    current_score = new_score
-                    if new_score < best_score:
-                        best_score = new_score
-                        best_trees = clone_trees(lattice_trees)
-                else:
-                    # get previous solution
-                    if move_type < len(primitive_trees):
-                        primitive_trees[move_type].set_params(*old_params)
-                    elif move_type == len(primitive_trees):
-                        a = old_a
-                        b = old_b
-                    else:
-                        for i, tree in enumerate(primitive_trees):
-                            x, y, _ = tree.get_params()
-                            tree.set_params(x, y, old_angles[i])
+            self.custom_log(temperature, current_score, best_score, no_of_rejections)
 
-                # logging
-                self.custom_log(temperature, iteration_at_temperature, current_score, best_score, t0)
-
-            # lower the temperature
             if self.cooling == "linear":
                 temperature_to_tmax = (self.Tmax - self.Tmin)
                 temperature_step = temperature_to_tmax / self.nsteps
                 temperature -= temperature_step
+
             all_scores.append(best_score)
-            with open("all_scores.txt", "w") as f:
-                f.write(str(all_scores))
+            self.save_score(temperature, best_score)
+        return best_trees
 
-        return best_score, best_trees
+    @profile
+    def save_score(self, temperature, score):
+        dir_name = Path("all_scores")
+        dir_name.mkdir(parents=True, exist_ok=True)
+        save_path = dir_name / f"scores_[{self.num_cell[0]}_{self.num_cell[1]}].csv"
+        with open(save_path, "a") as f:
+            f.write(f"{temperature},{score}\n")
 
+@profile
 def get_tree_list_side_length(tree_list: list[ChristmasTree]) -> Decimal:
     all_polygons = [tree.polygon for tree in tree_list]
     bounds = unary_union(all_polygons).bounds
     return Decimal(max(bounds[2] - bounds[0], bounds[3] - bounds[1])) / scale_factor
 
+@profile
 def get_total_score(dict_of_side_length: dict[str, Decimal]):
     score = 0
     for key, variable in dict_of_side_length.items():
@@ -314,10 +350,10 @@ def run_sa_for_cells(args):
     config_local = config["params"].copy()
     config_local["num_cell"] = [i, j]
     sa = SimulatedAnnealing(initial_trees, **config_local)
-    _, trees = sa.solve()
+    trees = sa.solve()
     return trees
 
-def evaluate_and_store(tree_list):
+def evaluate_and_store_best(tree_list):
     N = len(tree_list)
     side = get_tree_list_side_length(tree_list)
     if N not in new_trees:
@@ -359,15 +395,15 @@ if __name__ == '__main__':
     if multicore:
         with concurrent.futures.ProcessPoolExecutor() as executor:
             for trees in tqdm(executor.map(run_sa_for_cells, args_list), total=len(cell_combinations)):
-                evaluate_and_store(trees)
+                evaluate_and_store_best(trees)
     else:
         for cell_combination in tqdm(cell_combinations, total=len(cell_combinations)):
             i, j = cell_combination
             config_local = config["params"].copy()
             config_local["num_cell"] = [i, j]
             sa = SimulatedAnnealing(initial_trees, **config_local)
-            _, trees = sa.solve()
-            evaluate_and_store(trees)
+            trees = sa.solve()
+            evaluate_and_store_best(trees)
 
     def reduce_to_n_trees_inside_plot(tree_list, n):
         """Del farest trees"""
@@ -391,15 +427,13 @@ if __name__ == '__main__':
 
         available_plots = sorted(new_trees.keys())
 
-        for plot in range(1, max_n + 1):
-            # ready plot -> skip
-            if plot in new_trees:
-                continue
+        for plot in range(1, max_n):
 
             # all bigger plots
             biggers_M = [available_plot for available_plot in available_plots if available_plot > plot]
+
             if not biggers_M:
-                print("Brak większego plotu, coś nie tak, ustawić na większą siatkę")
+                print("Brak większego plotu, coś nie tak, ustawić na większą siatkę, albo 100% overlap")
                 continue
 
             best_side = None
@@ -414,8 +448,9 @@ if __name__ == '__main__':
                 if best_side is None or side < best_side:
                     best_side = side
                     best_trees = reduced
+                    best_M = bigger_M
 
-            print(f"Fill N={plot} using best of M={bigger_M}, side={best_side}")
+            print(f"Fill N={plot} using best of M={best_M}, side={best_side}")
 
             # zapisujemy najlepsze
             new_trees[plot] = (best_side, best_trees)
@@ -444,3 +479,18 @@ if __name__ == '__main__':
     save_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     df.to_csv(f"{save_time}_submission.csv", index=False)
     df.to_csv(f"new_submission.csv", index=False)
+
+    print("\n=== PROFILING RESULTS ===")
+    for name, stat in sorted(
+        FUNC_STATS.items(),
+        key=lambda x: x[1]["time"],
+        reverse=True
+    ):
+        avg = stat["time"] / stat["calls"]
+        print(
+            f"{name:40s} | "
+            f"calls={stat['calls']:8d} | "
+            f"total={stat['time']:8.2f}s | "
+            f"avg={avg*1000:8.3f} ms"
+        )
+
